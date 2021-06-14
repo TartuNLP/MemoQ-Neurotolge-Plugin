31508c4c09924326eabaaff84b3929ca2f56da35
Liisa <liisaratsep@gmail.com>
2021-06-14 09:50:56 +0300

    multi-version configurations, assembly info

--- TartuNLPPlugin/Properties/AssemblyInfo.cs
+++ TartuNLPPlugin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using MemoQ.Addins.Common.Framework;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TartuNLP")]
+[assembly: AssemblyTitle("TartuNLP MemoQ Plugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("TartuNLP")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCompany("University of Tartu")]
+[assembly: AssemblyProduct("TartuNLP MemoQ Plugin")]
+[assembly: AssemblyCopyright("Copyright © University of Tartu 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using MemoQ.Addins.Common.Framework;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// TODO update version upon new release
+[assembly: AssemblyVersion("2.0.1.*")]
 
 [assembly: Module(ModuleName = "TartuNLP", ClassName = "TartuNLP.TartuNLPPluginDirector")]
